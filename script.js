3d2015a26685cd18993c0137064fb163d2ce645c
Elizabeth Clawson <emclawson1@gmail.com>
2021-06-20 18:08:24 -0500

    added JavaScript element

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 const h2 = document.createElement('h2');
 h2.textContent = "This content added by JavaScript"; 
 document.querySelector('body').appendChild(h2); 
+
+var id = null;
+function myMove() {
+  var elem = document.getElementById("myAnimation");   
+  var pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 10);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++; 
+      elem.style.top = pos + 'px'; 
+      elem.style.left = pos + 'px'; 
+    }
+  }
+}
